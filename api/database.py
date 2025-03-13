5fe3a347481186c8bc1625eca482ef1a875c2cee
omkar234960 <omkarmishra2407@gmail.com>
2025-03-13 20:56:58 +0530

    Fixed SQLite database path

--- api/database.py
+++ api/database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./books.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'books.db')}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
